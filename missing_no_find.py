131d6eb9dbd551c29ed92b6f5e8f57dee8ba208f
Armanrbu <24rezams@rbunagpur.in>
2025-11-16 00:51:34 +0530

    Better solution

--- missing_no_find.py
+++ missing_no_find.py
@@ -1,9 +1,25 @@
 # brute force soln
 
-num = [4,2,1,0]
+# num = [4,2,1,0]
 
-n = len(num)
+# n = len(num)
 
-for i in range(0,n+1):
-    if i not in num:
-        print(i)
+# for i in range(0,n+1):
+#     if i not in num:
+#         print(i)
+
+
+#better soln using dic
+
+# n = len(num)
+# freq ={}
+
+# for i in range(0,n+1):
+#     freq[i] = 0
+
+# for na in num:
+#     freq[na] = 1
+
+# for k,v in freq.items():
+#     if v == 0:
+#         print(k)
