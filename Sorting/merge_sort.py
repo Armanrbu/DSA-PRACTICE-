f0b5754c73d90f78ffa833876fa0130cc39cb8c9
Armanrbu <24rezams@rbunagpur.in>
2025-11-19 23:55:57 +0530

    Update merge_sort.py

--- Sorting/merge_sort.py
+++ Sorting/merge_sort.py
@@ -1,1 +1,37 @@
+def merge_sort(arr):
+    # array size 1 ya 0 hai toh already sorted
+    if len(arr) <= 1:
+        return arr
 
+    mid = len(arr) // 2
+
+    # left aur right half sort kar lo
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+
+    # ab merge karna hai
+    return merge(left, right)
+
+
+def merge(left, right):
+    i = j = 0
+    result = []
+
+    # dono arrays ko compare karke sorted form banate jao
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    # agar left/right me kuch bacha ho toh directly chipka do
+    result.extend(left[i:])
+    result.extend(right[j:])
+
+    return result
+
+    nums = [12, 11, 13, 5, 6, 7]
+    sorted_nums = merge_sort(nums)
+    print("Sorted:", sorted_nums)
