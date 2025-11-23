4c26059bac5e3bf448fa7ea9f78e4d96d7543652
Armanrbu <24rezams@rbunagpur.in>
2025-11-23 23:17:27 +0530

    Update Max_consecutive_one.py

--- Max_consecutive_one.py
+++ Max_consecutive_one.py
@@ -1,4 +1,46 @@
-""" Step-by-Step Explanation
+
+"""
+Understanding Maximum Consecutive Ones Problem
+You are given a binary array, an array consisting only of 0s and 1s. Your task is to find the maximum number of consecutive 1s present in the array.
+
+Example 1
+Input: nums = [1, 1, 0, 1, 1, 1]
+We can see that:
+
+The first two 1s are followed by a 0.
+After that, there are three consecutive 1s.
+Output: 3
+The maximum number of continuous 1s is 3.
+
+Example 2
+Input: nums = [1, 0, 1, 1, 0, 1]
+In this case:
+
+There are no more than two 1’s in a row anywhere.
+Output: 2
+The best streak of 1s is [1, 1], so the result is 2.
+
+Example 3
+Input: nums = [0, 0, 0, 0]
+Since there are no 1s at all, there’s no sequence to count.
+
+Output: 0
+"""
+
+
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        cnt = 0
+        maxi = 0
+        for i in range(len(nums)):
+            if nums[i] == 1:
+                cnt += 1
+            else:
+                maxi = max(maxi, cnt)
+                cnt = 0
+        return max(maxi, cnt)
+        
+""" Step-by-Step code Explanation
 Initialization:
 cnt (Counter):
 Keeps track of the current number of consecutive 1s you’ve encountered.
@@ -16,21 +58,10 @@ After traversing the entire array, there might be a streak of 1s that wasn’t f
 Result:
 Return the value of maxi, which now holds the length of the longest consecutive 1s in the array."""
 
-
-class Solution:
-    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
-        cnt = 0
-        maxi = 0
-        for i in range(len(nums)):
-            if nums[i] == 1:
-                cnt += 1
-            else:
-                maxi = max(maxi, cnt)
-                cnt = 0
-        return max(maxi, cnt)
-
-
 """
+
+
+
 Time and Space Complexity
 
 Time Complexity: O(n):
