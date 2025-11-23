301b075ca6921150a2e9e1c7240117e1d30e607b
Armanrbu <24rezams@rbunagpur.in>
2025-11-23 23:09:12 +0530

    Restore findMaxConsecutiveOnes method implementation
    Restored the implementation of the findMaxConsecutiveOnes method.

--- Max_consecutive_one.py
+++ Max_consecutive_one.py
@@ -1,15 +1,3 @@
-class Solution:
-    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
-        cnt = 0
-        maxi = 0
-        for i in range(len(nums)):
-            if nums[i] == 1:
-                cnt += 1
-            else:
-                maxi = max(maxi, cnt)
-                cnt = 0
-        return max(maxi, cnt)
-        
 """ Step-by-Step Explanation
 Initialization:
 cnt (Counter):
@@ -27,3 +15,17 @@ Final Comparison:
 After traversing the entire array, there might be a streak of 1s that wasn’t followed by a 0. Therefore, perform one final comparison between cnt and maxi to ensure the longest streak is captured.
 Result:
 Return the value of maxi, which now holds the length of the longest consecutive 1s in the array."""
+
+
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        cnt = 0
+        maxi = 0
+        for i in range(len(nums)):
+            if nums[i] == 1:
+                cnt += 1
+            else:
+                maxi = max(maxi, cnt)
+                cnt = 0
+        return max(maxi, cnt)
+        
